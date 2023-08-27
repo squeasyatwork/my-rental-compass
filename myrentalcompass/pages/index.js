7b49d2bcd08d20665f8d3f239dada854ff008716
Zecheng <zpan0191@student.monash.edu>
2023-08-27 11:05:07 +1000

    complete introduction zone

--- myrentalcompass/pages/index.js
+++ myrentalcompass/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "./navbar.js";
+import Link from "next/link";
 
 export default function LandingPage() {
   return (
@@ -9,28 +10,41 @@ export default function LandingPage() {
         <meta name="description" content="Welcome to MyRentalCompass." />
       </Head>
 
-      <div className="font-inter flex flex-col m-1 h-screen">
-        <Navbar activePage="Home" />
+      <main>
+        <div className="font-inter flex flex-col h-screen">
+          <Navbar activePage="Home" />
 
-        <div className="relative" style={{ height: "50vh", marginTop: "6.6rem", zIndex: "-1"}}>
-          <div className=" absolute inset-0 bg-cover bg-center"
-            style = {{
-              backgroundImage: "url('/liveable-cities.jpeg')",
-              backgroundSize: "cover",
-              filter: "brightness(0.8)",
-              //zIndex: "-1",
-            }}
-          >
-            <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-              <h1 className="text-5xl font-bold">Everyone has a right for an</h1>
-              <br></br>
-              <h1 className="text-5xl font-bold">Affordable, Safe and Liveable housing</h1>
-              <br></br><br></br><br></br>
-              <h2 className="text-3xl font-normal">We are here to help you find the place you can call home</h2>
+          <div className="relative" style={{ height: "60vh", marginTop: "6.6rem", zIndex: "-1"}}>
+            <div className=" absolute inset-0 bg-cover bg-center"
+              style = {{
+                backgroundImage: "url('/liveable-cities.jpeg')",
+                backgroundSize: "cover",
+                //filter: "brightness(0.8)",
+                //zIndex: "-1",
+              }}
+            >
+              <div className="absolute inset-0 flex flex-col justify-between">
+                <div className=" flex flex-col items-center" style={{marginTop: "5rem"}}>
+                  <br></br><br></br>
+                  <h1 className="text-5xl font-bold">Everyone has a right for an</h1>
+                  <br></br>
+                  <h1 className="text-5xl font-bold">Affordable, Safe and Liveable housing</h1>
+                  <br></br><br></br><br></br>
+                  <h2 className="text-3xl font-normal">We are here to help you find the place you can call home</h2>
+                  <br></br><br></br>
+                  <Link href="/questionair">
+                    <button className="font-istok text-2xl font-semibold bg-yellow-400 text-black rounded-2xl p-8"> Find where to live </button>
+                  </Link>
+                  <br></br>
+                </div>
+                <div className=" flex flex-col items-end">
+                  <h2 className=" text-base font-normal">Picture credit: Australian Urban Observatory</h2>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
