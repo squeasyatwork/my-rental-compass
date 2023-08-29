1429bd50370a2b17128761032379ffe0370cc164
Abdullah Malek <131577597+squeasyatwork@users.noreply.github.com>
2023-08-29 11:42:49 +1000

    Update indextexthelper.js : adding a default export to resolve Amplify error

--- myrentalcompass/pages/helperpages/indextexthelper.js
+++ myrentalcompass/pages/helperpages/indextexthelper.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export const Section = ({
+export default const Section = ({
   id,
   imageSrc,
   altText,
