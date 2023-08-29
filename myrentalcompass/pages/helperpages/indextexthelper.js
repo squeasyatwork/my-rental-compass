3001291a59196e14cb1e855279eb2584a438f5b3
Abdullah Malek <131577597+squeasyatwork@users.noreply.github.com>
2023-08-29 12:03:38 +1000

    Update indextexthelper.js

--- myrentalcompass/pages/helperpages/indextexthelper.js
+++ myrentalcompass/pages/helperpages/indextexthelper.js
@@ -34,8 +34,33 @@ const Section = ({
       </div>
       <div className="text-center">
         <Link href={link}>
-          <button className="call-action-button">
-            {btnText}
+          <button className="call-action-button">{btnText}</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const ResourceSection = ({ id, imageSrc, altText, link }) => {
+  return (
+    <div
+      id={id}
+      className="flex flex-col justify-between w-96 h-80 border border-NavTextGray rounded-lg shadow-sm mx-auto p-4 pt-12"
+    >
+      <div className="flex-1 flex justify-center items-start">
+        <Image
+          src={imageSrc}
+          alt={altText}
+          width={150}
+          height={100}
+          loading="eager"
+          className="mx-auto"
+        />
+      </div>
+      <div className="mt-auto flex items-center justify-center pb-4">
+        <Link href={link}>
+          <button className="call-action-button text-NavTextGray font-bold bg-ResourceButtonYellow rounded-full w-40">
+            Read more
           </button>
         </Link>
       </div>
@@ -43,31 +68,4 @@ const Section = ({
   );
 };
 
-export const ResourceSection = ({ id, imageSrc, altText, link }) => {
-    return (
-      <div
-        id={id}
-        className="flex flex-col justify-between w-96 h-80 border border-NavTextGray rounded-lg shadow-sm mx-auto p-4 pt-12"
-      >
-        <div className="flex-1 flex justify-center items-start">
-          <Image
-            src={imageSrc}
-            alt={altText}
-            width={150}
-            height={100}
-            loading="eager"
-            className="mx-auto"
-          />
-        </div>
-        <div className="mt-auto flex items-center justify-center pb-4">
-          <Link href={link}>
-            <button className="call-action-button text-NavTextGray font-bold bg-ResourceButtonYellow rounded-full w-40">
-              Read more
-            </button>
-          </Link>
-        </div>
-      </div>
-    );
-  };
-  
-export default Section;
+export default {Section, ResourceSection};
