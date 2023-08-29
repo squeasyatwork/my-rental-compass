15f3e167b2d1366073057011ed0f73617d1300f6
amal0055 <amal0055@student.monash.edu>
2023-08-29 12:51:19 +1000

    pulling from develop before changing

--- myrentalcompass/pages/index.js
+++ myrentalcompass/pages/index.js
@@ -5,80 +5,12 @@ import React from "react";
 
 import Navbar from "~/pages/helperpages/navbar.js";
 import Footer from "./helperpages/footer.js";
-// import Section from "./helperpages/indextexthelper.js";
-// import ResourceSection from "./helperpages/otherresources.js";
+import { Section, ResourceSection } from "./helperpages/indextexthelper.js";
 
 const imageLoader = ({ src, width, quality }) => {
   return `https://develop.d1f77h13nbf5uz.amplifyapp.com/${src}?w=${width}&q=${
     quality || 75
   }`;
-};
-
-const Section = ({
-  id,
-  imageSrc,
-  altText,
-  subheading,
-  content,
-  link,
-  btnText,
-}) => {
-  return (
-    <div id={id} className="flex flex-col items-center mx-3 section-container">
-      <div className="bg-gray-200 w-96 h-44 flex rounded items-center justify-center mb-1">
-        <Image
-          loader={imageLoader}
-          src={imageSrc}
-          width={150}
-          height={100}
-          alt={altText}
-          loading="eager"
-          className="mx-auto"
-        />
-      </div>
-      <div className="yqa-subheading text-center w-96">{subheading}</div>
-      <div className="copywrite-content mx-2 my-4 text-justify w-96 mb-6">
-        {content.split("|||").map((line, index) => (
-          <React.Fragment key={index}>
-            {line}
-            {index !== content.split("|||").length - 1 && <br />}
-          </React.Fragment>
-        ))}
-      </div>
-      <div className="text-center">
-        <Link href={link}>
-          <button className="call-action-button">{btnText}</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
-
-const ResourceSection = ({ id, imageSrc, altText, link }) => {
-  return (
-    <div
-      id={id}
-      className="flex flex-col justify-between w-96 h-80 border border-NavTextGray rounded-lg shadow-sm mx-auto p-4 pt-12"
-    >
-      <div className="flex-1 flex justify-center items-start">
-        <Image
-          src={imageSrc}
-          alt={altText}
-          width={150}
-          height={100}
-          loading="eager"
-          className="mx-auto"
-        />
-      </div>
-      <div className="mt-auto flex items-center justify-center pb-4">
-        <Link href={link}>
-          <button className="call-action-button text-NavTextGray font-bold bg-ResourceButtonYellow rounded-full w-40">
-            Read more
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
 };
 
 export default function LandingPage() {
