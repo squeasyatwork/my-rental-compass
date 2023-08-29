9870ca535c5abdef06e3cdeb0fd3238f5402130a
amal0055 <amal0055@student.monash.edu>
2023-08-29 12:51:19 +1000

    pulling from develop before changing

---
+++ myrentalcompass/pages/helperpages/indextexthelper.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Section = ({
+  id,
+  imageSrc,
+  altText,
+  subheading,
+  content,
+  link,
+  btnText,
+}) => {
+  return (
+    <div id={id} className="flex flex-col items-center mx-3 section-container">
+      <div className="bg-gray-200 w-96 h-44 flex rounded items-center justify-center mb-1">
+        <Image
+          src={imageSrc}
+          width={150}
+          height={100}
+          alt={altText}
+          loading="eager"
+          className="mx-auto"
+        />
+      </div>
+      <div className="yqa-subheading text-center w-96">{subheading}</div>
+      <div className="copywrite-content mx-2 my-4 text-justify w-96 mb-6">
+        {content.split("|||").map((line, index) => (
+          <React.Fragment key={index}>
+            {line}
+            {index !== content.split("|||").length - 1 && <br />}
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="text-center">
+        <Link href={link}>
+          <button className="call-action-button">{btnText}</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Section;
---
+++ myrentalcompass/public/otherresources.js
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const imageLoader = ({ src, width, quality }) => {
+  return `https://develop.d1f77h13nbf5uz.amplifyapp.com/${src}?w=${width}&q=${
+    quality || 75
+  }`;
+};
+
+const ResourceSection = ({ id, imageSrc, altText, link }) => {
+  return (
+    <div
+      id={id}
+      className="flex flex-col justify-between w-96 h-80 border border-NavTextGray rounded-lg shadow-sm mx-auto p-4 pt-12"
+    >
+      <div className="flex-1 flex justify-center items-start">
+        <Image
+          loader={imageLoader}
+          src={imageSrc}
+          alt={altText}
+          width={150}
+          height={100}
+          loading="eager"
+          className="mx-auto"
+        />
+      </div>
+      <div className="mt-auto flex items-center justify-center pb-4">
+        <Link href={link}>
+          <button className="call-action-button text-NavTextGray font-bold bg-ResourceButtonYellow rounded-full w-40">
+            Read more
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ResourceSection;
