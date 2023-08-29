170ec76c3936d859a584ee5cbff2fc505f0ba073
Abdullah Malek <131577597+squeasyatwork@users.noreply.github.com>
2023-08-29 11:54:12 +1000

    Update indextexthelper.js

--- myrentalcompass/pages/helperpages/indextexthelper.js
+++ myrentalcompass/pages/helperpages/indextexthelper.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export default const Section = ({
+export const Section = ({
   id,
   imageSrc,
   altText,
@@ -34,9 +34,7 @@ export default const Section = ({
       </div>
       <div className="text-center">
         <Link href={link}>
-          <button className="call-action-button">
-            {btnText}
-          </button>
+          <button className="call-action-button">{btnText}</button>
         </Link>
       </div>
     </div>
@@ -44,30 +42,28 @@ export default const Section = ({
 };
 
 export const ResourceSection = ({ id, imageSrc, altText, link }) => {
-    return (
-      <div
-        id={id}
-        className="flex flex-col justify-between w-96 h-80 border border-NavTextGray rounded-lg shadow-sm mx-auto p-4 pt-12"
-      >
-        <div className="flex-1 flex justify-center items-start">
-          <Image
-            src={imageSrc}
-            alt={altText}
-            width={150}
-            height={100}
-            loading="eager"
-            className="mx-auto"
-          />
-        </div>
-        <div className="mt-auto flex items-center justify-center pb-4">
-          <Link href={link}>
-            <button className="call-action-button text-NavTextGray font-bold bg-ResourceButtonYellow rounded-full w-40">
-              Read more
-            </button>
-          </Link>
-        </div>
+  return (
+    <div
+      id={id}
+      className="flex flex-col justify-between w-96 h-80 border border-NavTextGray rounded-lg shadow-sm mx-auto p-4 pt-12"
+    >
+      <div className="flex-1 flex justify-center items-start">
+        <Image
+          src={imageSrc}
+          alt={altText}
+          width={150}
+          height={100}
+          loading="eager"
+          className="mx-auto"
+        />
       </div>
-    );
-  };
-  // ignore
-
+      <div className="mt-auto flex items-center justify-center pb-4">
+        <Link href={link}>
+          <button className="call-action-button text-NavTextGray font-bold bg-ResourceButtonYellow rounded-full w-40">
+            Read more
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
