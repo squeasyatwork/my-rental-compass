50b7b2726d05c3b6a440bf04558fc83c4ed840f3
Abdullah Malek <131577597+squeasyatwork@users.noreply.github.com>
2023-08-29 12:19:14 +1000

    Update indextexthelper.js : separating Section and ResourceSection components

--- myrentalcompass/pages/helperpages/indextexthelper.js
+++ myrentalcompass/pages/helperpages/indextexthelper.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export const Section = ({
+const Section = ({
   id,
   imageSrc,
   altText,
@@ -41,31 +41,4 @@ export const Section = ({
   );
 };
 
-const ResourceSection = ({ id, imageSrc, altText, link }) => {
-  return (
-    <div
-      id={id}
-      className="flex flex-col justify-between w-96 h-80 border border-NavTextGray rounded-lg shadow-sm mx-auto p-4 pt-12"
-    >
-      <div className="flex-1 flex justify-center items-start">
-        <Image
-          src={imageSrc}
-          alt={altText}
-          width={150}
-          height={100}
-          loading="eager"
-          className="mx-auto"
-        />
-      </div>
-      <div className="mt-auto flex items-center justify-center pb-4">
-        <Link href={link}>
-          <button className="call-action-button text-NavTextGray font-bold bg-ResourceButtonYellow rounded-full w-40">
-            Read more
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
-};
-
-export default ResourceSection;
+export default Section;
