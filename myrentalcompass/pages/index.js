b19d4f9b8f5be7a128747cccc59395f2e3e72425
Brigette Maher <bv1232@outlook.com.au>
2023-08-31 00:07:18 +1000

    add react import

--- myrentalcompass/pages/index.js
+++ myrentalcompass/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import React from "react";
 
 import Navbar from "./helperpages/navbar.js";
 import Footer from "./helperpages/footer.js";
