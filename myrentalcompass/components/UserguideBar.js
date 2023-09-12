90bbf966d57fcce0b9b7a0a6e97e319c9f079cb0
lebanap <abelpan2022@gmail.com>
2023-09-12 15:28:08 +1000

    fix toggle display logic

--- myrentalcompass/components/UserguideBar.js
+++ myrentalcompass/components/UserguideBar.js
@@ -2,11 +2,26 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 
 const UserguideBar = () => {
-  const [showDetails, setShowDetails] = useState(false);
-
-  const toggleDetails = () => {
-    setShowDetails(!showDetails);
-  };
+    const [showDetails1, setShowDetails1] = useState(false);
+    const [showDetails2, setShowDetails2] = useState(false);
+    const [showDetails3, setShowDetails3] = useState(false);
+    const [showDetails4, setShowDetails4] = useState(false);
+  
+    const toggleDetails1 = () => {
+      setShowDetails1(!showDetails1);
+    };
+  
+    const toggleDetails2 = () => {
+      setShowDetails2(!showDetails2);
+    };
+  
+    const toggleDetails3 = () => {
+      setShowDetails3(!showDetails3);
+    };
+  
+    const toggleDetails4 = () => {
+      setShowDetails4(!showDetails4);
+    };
 
   return (
     <div className="mx-auto flex flex-col">
@@ -19,18 +34,8 @@ const UserguideBar = () => {
                 height={100}
                 className="rounded-xl"
                 />
-            </div>
-            <div className=' flex flex-col w-2 h-full bg-black'></div>
-            <div className='flex'>
-                <Image
-                src="/information-icon.svg"
-                alt="information"
-                width={100}
-                height={100}
-                className="rounded-xl"
-                />
                 <button
-                onClick={toggleDetails}
+                onClick={toggleDetails1}
                 className=" font-medium text-2xl px-3 py-1 rounded-md"
                 >
                 1.Before you leave...
@@ -38,7 +43,7 @@ const UserguideBar = () => {
             </div>
             <div className='flex'>
                 <button
-                onClick={toggleDetails}
+                onClick={toggleDetails2}
                 className=" font-medium text-2xl px-3 py-1 rounded-md"
                 >
                 2.When you arrive...
@@ -60,7 +65,7 @@ const UserguideBar = () => {
                 className="rounded-xl"
                 />
                 <button
-                onClick={toggleDetails}
+                onClick={toggleDetails3}
                 className=" font-medium text-2xl px-3 py-1 rounded-md"
                 >
                 3.Found a property...
@@ -84,14 +89,53 @@ const UserguideBar = () => {
                     />
                 </div>
                 <button
-                onClick={toggleDetails}
+                onClick={toggleDetails4}
                 className=" font-medium text-2xl px-3 py-1 rounded-md"
                 >
                 4. Making an appointment...
                 </button>
             </div>
         </div>
-        {showDetails && (
+        {showDetails1 && (
+            <div className="mt-4">
+            <div className="cursor-pointer">
+                <h2 className="text-lg font-semibold">Section 1</h2>
+                <p>Details about Section 1...</p>
+            </div>
+            <div className="cursor-pointer mt-2">
+                <h2 className="text-lg font-semibold">Section 2</h2>
+                <p>Details about Section 2...</p>
+            </div>
+            {/* Add more sections as needed */}
+            </div>
+        )}
+        {showDetails2 && (
+            <div className="mt-4">
+            <div className="cursor-pointer">
+                <h2 className="text-lg font-semibold">Section 1</h2>
+                <p>Details about Section 1...</p>
+            </div>
+            <div className="cursor-pointer mt-2">
+                <h2 className="text-lg font-semibold">Section 2</h2>
+                <p>Details about Section 2...</p>
+            </div>
+            {/* Add more sections as needed */}
+            </div>
+        )}
+        {showDetails3 && (
+            <div className="mt-4">
+            <div className="cursor-pointer">
+                <h2 className="text-lg font-semibold">Section 1</h2>
+                <p>Details about Section 1...</p>
+            </div>
+            <div className="cursor-pointer mt-2">
+                <h2 className="text-lg font-semibold">Section 2</h2>
+                <p>Details about Section 2...</p>
+            </div>
+            {/* Add more sections as needed */}
+            </div>
+        )}
+        {showDetails4 && (
             <div className="mt-4">
             <div className="cursor-pointer">
                 <h2 className="text-lg font-semibold">Section 1</h2>
