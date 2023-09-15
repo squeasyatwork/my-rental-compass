7ababdc22a3f612ae3055367a6abc3e8de0f50cd
lebanap <abelpan2022@gmail.com>
2023-09-15 17:59:53 +1000

    refine what you need to know page

--- myrentalcompass/pages/rights.js
+++ myrentalcompass/pages/rights.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "./helperpages/navbar.js";
 import Footer from "./helperpages/footer.js";
 import UserguideBar from "~/components/UserguideBar.js";
+import Image from "next/image";
 
 export default function Rights() {
   return (
@@ -16,38 +17,39 @@ export default function Rights() {
 
         <div className="relative h-3/5 w-full">
           <img
-            src="/liveable-cities.jpeg"
+            src="/businesswoman.jpeg"
             alt="Description of the image"
             className="absolute inset-0 object-cover object-center w-full h-full filter brightness-60 z-0"
           />
           <div className="relative flex flex-col justify-between h-full text-BackgroundWhite">
             <div className="flex flex-col justify-center my-44">
               <h1 className="text-5xl font-bold text-center">
-                Knowing what to do to apply for a rental can be
+               As a tenant, you have rights and to live in a
               </h1>
               <h1 className="text-5xl font-bold text-center mt-4">
-                confusing!
+                safe, secure and quiet environment
               </h1>
               <h1 className="text-5xl font-bold text-center mt-4">
-                Follow our guide to take you on the journey of
+                that is managed in accordance with the law
               </h1>
-              <h2 className="text-5xl font-bold text-center mt-4">
-                applyng for a property
-              </h2>
             </div>
           </div>
         </div>
-        <div className=" flex flex-col bg-white px-6 pb-20 items-center p-8">
-          <div className="relative flex flex-col justify-between h-full text-HeadingTextGray">
-            <h1 className="text-5xl font-bold text-center">
-              Follow our step-by-step guide below to help you
-            </h1>
-            <h1 className="text-5xl font-bold text-center mt-4">
-              understand the rental process
-            </h1>
-          </div>
-          <div className="relative my-8">
-            <UserguideBar />
+        <div className=" flex flex-col bg-white px-6 items-center p-8">
+          <div className="flex justify-center">
+            <div className="flex flex-col items-center justify-center text-HeadingTextGray font-bold text-4xl">
+              <h2>Understand your </h2>
+              <h2>rights and </h2>
+              <h2>responsibilities</h2>
+            </div>
+            <div className="flex">
+              <Image
+                src="/woman.gif"
+                alt="girl"
+                width={300}
+                height={300}
+              />
+            </div>
           </div>
         </div>
         <Footer />
