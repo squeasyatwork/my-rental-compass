fe75257a69559b88194ffd7590104a0160fc8728
bee-w <bv1232@outlook.com.au>
2023-09-18 19:01:05 +1000

    recommendations linked to new data  and formula updated

--- myrentalcompass/pages/api/liveablesuburbs.js
+++ myrentalcompass/pages/api/liveablesuburbs.js
@@ -1,25 +1,110 @@
-import prisma from '../../lib/prisma.js';
+import prisma from "~/lib/prisma";
 
-export default async function handler(req, res) {
-  try {
-    const rentLimit = parseFloat(req.query.rent); // Extract the rent query parameter and parse it as a float since you're using Decimal.
+export default async function getRankedLiveability(req, res) {
+  // Check for valid request and HTTP method
+  if (!req.query) {
+    return res.status(200).json(null);
+  }
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
 
-    if (!rentLimit || isNaN(rentLimit)) {
-      return res.status(400).json({ error: "Invalid rent value provided." });
+  const contextQuery = req.query;
+
+  // Safely parse query parameters
+  const rent = parseFloat(contextQuery.rent);
+  // const affordability = parseFloat(contextQuery.affordability);
+  const transport = parseFloat(contextQuery.transport);
+  const park = parseFloat(contextQuery.park);
+  const crime = parseFloat(contextQuery.crime);
+  const road = parseFloat(contextQuery.road);
+  const university = contextQuery.university;
+
+  // Map ratings to weightages
+  const ratingArray = [1, 2, 3, 4, 5];
+  const weightageArray = [1.0, 1.5, 2.0, 2.5, 3.0];
+  // const rentWeightage = weightageArray[ratingArray.indexOf(affordability)];
+  const transportWeightage = weightageArray[ratingArray.indexOf(transport)];
+  const parkWeightage = weightageArray[ratingArray.indexOf(park)];
+  const crimeWeightage = weightageArray[ratingArray.indexOf(crime)];
+  const roadWeightage = weightageArray[ratingArray.indexOf(road)];
+
+  // Function to rank suburbs based on different criteria
+  const rankSuburbs = async (array) => {
+    for (let item of array) {
+      let uniScore = 1; // Default value in case university data isn't found
+      const uniData = await prisma.university.findFirst({
+        where: {
+          uni_to_suburb: item.suburb,
+          university: university,
+        },
+      });
+
+      if (uniData) {
+        switch (uniData.distance_score) {
+          case 5:
+            uniScore = 5;
+            break;
+          case 4:
+            uniScore = 2.5;
+            break;
+          case 3:
+            uniScore = 2;
+            break;
+          case 2:
+            uniScore = 1.5;
+            break;
+          default:
+            break;
+        }
+      }
+
+      const rentScore = item.average_rent <= rent ? 5 :
+                        item.average_rent <= rent * 1.05 ? 2.5 :
+                        item.average_rent <= rent * 1.10 ? 2 :
+                        item.average_rent <= rent * 1.20 ? 1.5 : 1;
+
+      const liveability = await prisma.liveability.findFirst({
+        where: {
+          liveability_suburb: item.suburb,
+        },
+      });
+
+      if (liveability) {
+        item.liveability_score =
+          liveability.ptv_score * transportWeightage +
+          liveability.openspace_score * parkWeightage +
+          liveability.crime_score * crimeWeightage +
+          liveability.crash_score * roadWeightage +
+          uniScore + rentScore;
+      }
     }
 
-    // Fetch data from your database using Prisma with a rent filter
-    const suburbs = await prisma.data_by_suburb.findMany({
-      where: {
-        average_rent: {
-          lte: rentLimit, // lte stands for 'less than or equal to'
-        },
-      },
-    });
+    return array.sort((a, b) => b.liveability_score - a.liveability_score);
+  };
 
-    res.status(200).json(suburbs);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  const suburbData = await prisma.suburb.findMany();
+  let rankedSuburbs = await rankSuburbs(suburbData);
+
+  // Normalize the scores
+  const maxScore = Math.max(
+    ...rankedSuburbs.map((item) => item.liveability_score)
+  );
+  const minScore = Math.min(
+    ...rankedSuburbs.map((item) => item.liveability_score)
+  );
+  rankedSuburbs = rankedSuburbs.map((item) => ({
+    ...item,
+    liveability_score:
+      (item.liveability_score - minScore) / (maxScore - minScore),
+  }));
+
+  res.status(200).json(
+    rankedSuburbs.map((item) => ({
+      suburb: item.suburb,
+      liveability_score: item.liveability_score,
+      lga: item.lga,
+      average_rent: item.average_rent,
+    }))
+  );
 }
