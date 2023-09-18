c81ccceb78d0fb86d2de48bad79ab9a0cf920d39
bee-w <bv1232@outlook.com.au>
2023-09-18 16:12:50 +1000

    t

--- myrentalcompass/pages/api/liveablesuburbs.js
+++ myrentalcompass/pages/api/liveablesuburbs.js
@@ -1,103 +1,25 @@
-import prisma from "~/lib/prisma";
+import prisma from '../../lib/prisma.js';
 
-export default async function getRankedLiveability(req, res) {
-  // Check for valid request and HTTP method
-  if (!req.query) {
-    return res.status(200).json(null);
-  }
-  if (req.method !== "GET") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+export default async function handler(req, res) {
+  try {
+    const rentLimit = parseFloat(req.query.rent); // Extract the rent query parameter and parse it as a float since you're using Decimal.
 
-  const contextQuery = req.query;
-
-  // Safely parse query parameters
-  const rent = parseFloat(contextQuery.rent);
-  const affordability = parseFloat(contextQuery.affordability);
-  const transport = parseFloat(contextQuery.transport);
-  const park = parseFloat(contextQuery.park);
-  const crime = parseFloat(contextQuery.crime);
-  const road = parseFloat(contextQuery.road);
-  const university = contextQuery.university;
-
-  // Map ratings to weightages
-  const ratingArray = [1, 2, 3, 4, 5];
-  const weightageArray = [1.0, 1.5, 2.0, 2.5, 3.0];
-  const rentWeightage = weightageArray[ratingArray.indexOf(affordability)];
-  const transportWeightage = weightageArray[ratingArray.indexOf(transport)];
-  const parkWeightage = weightageArray[ratingArray.indexOf(park)];
-  const crimeWeightage = weightageArray[ratingArray.indexOf(crime)];
-  const roadWeightage = weightageArray[ratingArray.indexOf(road)];
-
-  // Function to rank suburbs based on different criteria
-  const rankSuburbs = async (array) => {
-    for (let item of array) {
-      let uniScore = 1; // Default value in case university data isn't found
-      const uniData = await prisma.suburb_uni_score.findFirst({
-        where: {
-          suburb_uni: item.suburb,
-          university: university,
-        },
-      });
-
-      if (uniData) {
-        switch (uniData.distance_score) {
-          case 5:
-            uniScore = 3;
-            break;
-          case 4:
-            uniScore = 2.5;
-            break;
-          case 3:
-            uniScore = 2;
-            break;
-          case 2:
-            uniScore = 1.5;
-            break;
-          default:
-            break;
-        }
-      }
-
-      const rentScore = item.average_rent <= rent ? 3 :
-                        item.average_rent <= rent * 1.05 ? 2.5 :
-                        item.average_rent <= rent * 1.10 ? 2 :
-                        item.average_rent <= rent * 1.20 ? 1.5 : 1;
-
-      item.liveability_score =
-        item.rent_score * rentWeightage +
-        item.transport_score * transportWeightage +
-        item.openspace_score * parkWeightage +
-        item.crime_score * crimeWeightage +
-        item.saferoads_score * roadWeightage +
-        uniScore + rentScore;
+    if (!rentLimit || isNaN(rentLimit)) {
+      return res.status(400).json({ error: "Invalid rent value provided." });
     }
 
-    return array.sort((a, b) => b.liveability_score - a.liveability_score);
-  };
+    // Fetch data from your database using Prisma with a rent filter
+    const suburbs = await prisma.data_by_suburb.findMany({
+      where: {
+        average_rent: {
+          lte: rentLimit, // lte stands for 'less than or equal to'
+        },
+      },
+    });
 
-  const liveabilityData = await prisma.liveability_data.findMany();
-  let rankedSuburbs = await rankSuburbs(liveabilityData);
-
-  // Normalize the scores
-  const maxScore = Math.max(
-    ...rankedSuburbs.map((item) => item.liveability_score)
-  );
-  const minScore = Math.min(
-    ...rankedSuburbs.map((item) => item.liveability_score)
-  );
-  rankedSuburbs = rankedSuburbs.map((item) => ({
-    ...item,
-    liveability_score:
-      (item.liveability_score - minScore) / (maxScore - minScore),
-  }));
-
-  res.status(200).json(
-    rankedSuburbs.map((item) => ({
-      suburb: item.suburb,
-      liveability_score: item.liveability_score,
-      lga: item.lga,
-      average_rent: item.average_rent,
-    }))
-  );
+    res.status(200).json(suburbs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
--- myrentalcompass/pages/recommendations.js
+++ myrentalcompass/pages/recommendations.js
@@ -1,43 +1,26 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import Router from "next/router";
-import { Box } from "@mui/material";
 import Head from "next/head";
-import dynamic from "next/dynamic";
-import { useState } from "react";
-
-import { RentSlider, LiveabilitySliders } from "~/components/Sliders.js";
-import UniversityDropdown from "~/components/Dropdown";
-import Navbar from "./helperpages/navbar.js";
-import Footer from "./helperpages/footer.js";
-import { style } from "d3";
-
-const DynamicBasicMap = dynamic(() => import("~/components/BasicMap"), {
-  ssr: false,
-});
-
-export const config = {
-  runtime: "nodejs",
-};
 
 export const getServerSideProps = async (context) => {
   if (context.query) {
     let contextQuery = context.query;
     let reqQuery = new URLSearchParams(contextQuery);
 
-    let dbResponse = await fetch(
-      process.env.API_URL + "/api/liveablesuburbs?" + reqQuery,
-      {
-        method: "GET",
-      }
-    );
+    const apiUrl = process.env.API_URL + "/api/liveablesuburbs?" + reqQuery;
+    console.log("Constructed API URL:", apiUrl);
 
-    // Check if the response is valid and is JSON before attempting to parse
-    if (
-      dbResponse.ok &&
-      dbResponse.headers.get("content-type").includes("application/json")
-    ) {
+    let dbResponse = await fetch(apiUrl, { method: "GET" });
+
+    console.log('DB Response:', dbResponse);
+
+    if (dbResponse.ok && dbResponse.headers.get("content-type").includes("application/json")) {
       let rankedSuburbs = await dbResponse.json();
+      console.log('Ranked Suburbs:', rankedSuburbs);
       return { props: { rankedSuburbs, contextQuery } };
+    } else {
+      console.error('Error fetching from API:', dbResponse.statusText);
+      return { props: { rankedSuburbs: null, contextQuery: {} } };
     }
   }
 
@@ -45,12 +28,12 @@ export const getServerSideProps = async (context) => {
 };
 
 export default function Recommendations({
-  rankedSuburbs = null,
+  rankedSuburbs = [],
   contextQuery = {},
 }) {
   const router = Router.useRouter();
 
-  const [inputValues, setInputValues] = React.useState({
+  const [inputValues, setInputValues] = useState({
     rent: contextQuery.rentChoice || 50,
     affordability: contextQuery.affordabilityChoice || 3,
     transport: contextQuery.transportChoice || 3,
@@ -60,22 +43,14 @@ export default function Recommendations({
     university: contextQuery.uniChoice || "",
   });
 
-  const [selectedFeature, setSelectedFeature] = React.useState(null);
-  const [isPanelOpen, setIsPanelOpen] = React.useState(false);
-  // initialize mouse position
-  const [boxPosition, setBoxPosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    console.log('Ranked Suburbs State:', rankedSuburbs);
+  }, [rankedSuburbs]);
 
   const handleInputChange = (e) => {
     setInputValues({
       ...inputValues,
       [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSliderChange = (name) => (e, value) => {
-    setInputValues({
-      ...inputValues,
-      [name]: value,
     });
   };
 
@@ -86,185 +61,34 @@ export default function Recommendations({
     });
   };
 
-  const topTenSuburbs = rankedSuburbs ? rankedSuburbs.slice(0, 10) : [];
-
-  // The following code is for the selected feature details box
-  const handleMouseClick = (event) => {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    console.log(`[${mouseX}, ${mouseY}]`)
-
-    setBoxPosition({ x: mouseX, y: mouseY });
-  };
-
   return (
     <>
       <Head>
-        <title>MyRentalCompass | Recommendations</title>
-        <meta name="description" content="Discover potential suburbs." />
+        <title>API Test Page</title>
       </Head>
-      <main className="font-inter flex flex-col h-screen">
-        <Navbar activePage="Find where to live" />
+      <main>
+        <h1>Testing API Interaction</h1>
 
-        <section className="flex flex-col bg-ResourceButtonYellow md:flex-col sm:flex-col items-start justify-center pt-5 pl-12 pb-2 text-left">
-          <div className="font-bold text-4xl text-black">
-            <h1>Here are the Melbourne surburbs that we think is suitable for you</h1>
-          </div>
-        </section>
-        <section className="flex-grow w-full bg-ResourceButtonYellow flex flex-col items-center justify-center text-NavTextGray p-4">
-          <Box
-            my="14px"
-            bgcolor="#fff"
-            borderRadius="12px"
-            padding="1rem"
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              width: "80%",
-              justifyContent: "space-between",
-              boxShadow: "0 4px 6px rgb(0 0 0 / 0.1)",
-            }}
-          >
+        <div>
+          <h2>Inputs</h2>
+          <input
+            type="text"
+            name="rent"
+            value={inputValues.rent}
+            onChange={handleInputChange}
+            placeholder="Rent"
+          />
+          <button onClick={sendInput}>Send Input</button>
+        </div>
 
-            <Box
-              sx={{
-                width: "20%",
-                display: "flex",
-                flexDirection: "column",
-                gap: "20px",
-                marginRight: "2rem",
-                padding: "1rem",
-              }}
-            >
-              <div className=" font-bold text-2xl">
-                <h1>Updated Selection</h1>
-              </div>
-              {/* Rental and liveability sliders */}
-              <RentSlider
-                handleChoice={handleSliderChange("rent")}
-                defaultArg={inputValues.rent}
-              />
-              <LiveabilitySliders
-                inputValues={inputValues}
-                handleSliderChange={handleSliderChange}
-              />
-
-              {/* University dropdown */}
-              <UniversityDropdown
-                value={{ label: inputValues.university }}
-                onChange={(event, newValue) =>
-                  handleInputChange({
-                    target: {
-                      name: "university",
-                      value: newValue ? newValue.label : "",
-                    },
-                  })
-                }
-              />
-
-              <button className="text-lg md:text-lg lg:text-lg font-bold call-action-button" onClick={sendInput}>
-                Update Result
-              </button>
-            </Box>
-
-            <Box
-              sx={{
-                width: "80%",
-                position: "relative",
-                display: "flex",
-                flexDirection: "column",
-                gap: "20px",
-              }}
-            >
-              {/* Dynamic map rendering */}
-              <DynamicBasicMap
-                recommendations={true}
-                data={rankedSuburbs}
-                setSelectedFeature={setSelectedFeature}
-                onMouseEnter={(feature) => setSelectedFeature(feature)}
-                onMouseLeave={() => setSelectedFeature(null)}
-                onClick={handleMouseClick}
-              />
-
-              {/* Top 10 Suburbs panel */}
-              <Box
-                bgcolor="#fff"
-                padding="1rem"
-                borderRadius="10px"
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  position: "absolute",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  right: 0,
-                  top: "10px",
-                  width: "37%",
-                  boxShadow: "0 4px 6px rgb(0 0 0 / 0.1)",
-                  maxHeight: "70vh",
-                  //overflowY: "scroll",
-                  zIndex: 1000,
-                }}
-              >
-                {/* Panel toggle button */}
-                <button className=" text-base md:text-base lg:text-base font-bold call-action-button"
-                  onClick={() => setIsPanelOpen(!isPanelOpen)
-                  }
-                >
-                  {isPanelOpen ? '▼ Hide Top 10 Suburbs' : '▶ See Top 10 Suburbs'}
-                </button>
-
-                {isPanelOpen && (
-                  <><h3 className=" font-istok text-lg text-center font-bold mt-2">Suburb Recommendations For You</h3><table className="mx-auto">
-                    <thead>
-                      <tr>
-                        <th className=" text-sm font-medium px-2 border-b-2">Rank</th>
-                        <th className=" text-sm font-medium px-2 border-b-2">Score</th>
-                        <th className=" text-sm font-medium px-2 border-b-2">Suburbs</th>
-                        <th className=" text-sm font-medium px-2 border-b-2">Rent ($/week)</th>
-                      </tr>
-                    </thead>
-                    <tbody className=" text-sm font-normal items-center justify-center text-center">
-                      {topTenSuburbs.map((suburb, index) => (
-                        <tr key={suburb.suburb} style={{ margin: "1rem" }}>
-                          <td className="px-2 py-2">{index + 1}</td>
-                          <td className="px-2 py-2">{(suburb.liveability_score * 100).toFixed(2)}</td>
-                          <td className="px-2 py-2">{suburb.suburb}</td>
-                          <td className="px-2 py-2">${suburb.average_rent}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table></>)}
-              </Box>
-
-
-              {/* Selected feature details 
-                cannot the box cannot be displayed near the mouse*/}
-              {selectedFeature && (
-                <div
-                  style={{
-                    backgroundColor: "#fff",
-                    padding: "1rem",
-                    borderRadius: "12px",
-                    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-                    zIndex: 1000,
-                    position: 'absolute',
-                    left: `${boxPosition.x}px`,
-                    top: `${boxPosition.y}px`,
-                  }}
-                >
-                  <p>Name: {selectedFeature.suburb}</p>
-                  <p>Council: {selectedFeature.lga}</p>
-                  <p>
-                    Liveability Score:{" "}
-                    {(selectedFeature.liveability_score * 100).toFixed(2)}%
-                  </p>
-                </div>
-              )}
-            </Box>
-          </Box>
-        </section>
-        <Footer />
+        <div>
+          <h2>Output</h2>
+          {rankedSuburbs && rankedSuburbs.length > 0 ? (
+            <pre>{JSON.stringify(rankedSuburbs, null, 2)}</pre>
+          ) : (
+            <p>No data received from API.</p>
+          )}
+        </div>
       </main>
     </>
   );
