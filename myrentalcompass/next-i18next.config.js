b2454a23810e8d1c5f638495c6c619aaf68cc5e0
squeasyatwork <amal0055@student.monash.edu>
2023-10-08 15:08:25 +1100

    Added Chinese as well. Path to check --> myrentalcompass.me/zh/resources

--- myrentalcompass/next-i18next.config.js
+++ myrentalcompass/next-i18next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     i18n: {
         // all the locales supported in the application
-        locales: ['en', 'hi', 'ms'],
+        locales: ['en', 'hi', 'ms', 'zh'],
         // the default locale to be used when visiting
         // a non-localized route (e.g. `/about`)   
         defaultLocale: 'en'
